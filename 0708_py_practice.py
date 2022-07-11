bafa793dc4ea6884cac841ccfe21b88428ec7147
unknown <hyunicho62@gmail.com>
2022-07-11 16:32:12 +0900

    0711 python study - variables if while

--- 0708_py_practice.py
+++ 0708_py_practice.py
@@ -1,4 +1,8 @@
+#%%
 aa = []
 for i in range(100):
     aa.append(0)
 print(aa)
+
+bb = [0 for i in range(0,100)]
+print(bb)
---
+++ [Book]do it, jump to python/0708_py_practice.py
@@ -0,0 +1,4 @@
+aa = []
+for i in range(100):
+    aa.append(0)
+print(aa)
