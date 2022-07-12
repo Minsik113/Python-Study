dfc2853534d5d34c23aade5d5d3b00e7c7bd4519
unknown <hyunicho62@gmail.com>
2022-07-12 17:13:44 +0900

    0712 - for문, function 실습

--- 0708_py_practice.py
+++
@@ -1,8 +0,0 @@
-#%%
-aa = []
-for i in range(100):
-    aa.append(0)
-print(aa)
-
-bb = [0 for i in range(0,100)]
-print(bb)
---
+++ list_comprehension_.py
@@ -0,0 +1,23 @@
+'''
+map, list comprehension, loop 속도 체크
+
+'''
+import random
+import timeit
+TAX_RATE = .08
+txns = [random.randrange(100) for _ in range(100000)]
+def get_price(txn):
+    return txn * (1 + TAX_RATE)
+def get_prices_with_map():
+    return list(map(get_price, txns))
+
+def get_prices_with_comprehension():
+    return [get_price(txn) for txn in txns]
+def get_prices_with_loop():
+    prices = []
+    for txn in txns:
+        prices.append(get_price(txn))
+    return prices
+print(timeit.timeit(get_prices_with_map, number=100))
+print(timeit.timeit(get_prices_with_comprehension, number=100))
+print(timeit.timeit(get_prices_with_loop, number=100))
